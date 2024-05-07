ce3d95826f7de7730ab7724490eac96bd50a5fd6
Alan Jones <alan.jones@sciencelogic.com>
2024-05-07 08:25:37 -0700

    fix execute-sql by running it before cd into /changelog

--- changelog/entrypoint.sh
+++ changelog/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -x
 export PATH=/opt/java/openjdk/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-liquibase -execute-sql --sql="create schema if not exists myschema"
+liquibase execute-sql --sql="create schema if not exists myschema"
+cd /changelog
+liquibase release-locks
 liquibase update
